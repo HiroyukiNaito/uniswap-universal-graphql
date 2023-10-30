d714bb3da5ebf742f9437ce589f9ea8e189d4b28
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2023-10-30 20:24:15 +0900

    Added pino-http

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { createYoga, createSchema } = require('graphql-yoga');
 const { createServer } = require('node:http');
+const logger = require('pino-http')();
 
 //  Loading Schema and Resolver
 const typeDefs = require('./graphQL/schema/index');
 const resolvers = require('./graphQL/resolver/index');
 const schema = createSchema({ typeDefs, resolvers });
+
 
 // Mongo DB server settings
 const mongo_server = process.env.MONGODB_SERVER;
@@ -18,10 +20,10 @@ const database =`mongodb://${user}:${pass}@${mongo_server}:27017/${db}`;
 console.log('Try to connect: ', database);
 mongoose.connect(database)
     .then(() => {
-        console.log('Connection to DB successful');
+        logger.info('Connection to DB successful');
     })
     .catch(err => {
-        console.log('Db connection error====', err);
+        logger.error(err, 'DB Connection Error!');
     });
 
 // Create Yoga GraphQL Server
