a3647023e96cb0f59672b4ea78bf058004e664cc
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2023-12-01 21:00:21 +0900

    Added websocket server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { createYoga, createSchema } = require('graphql-yoga');
 const { createServer } = require('node:http');
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+
 const pino = require('pino');
 const logger = pino({
   level: process.env.PINO_LOG_LEVEL ?? 'info',
@@ -35,8 +38,53 @@ mongoose.connect(database)
     });
 
 // Create Yoga GraphQL Server
-const yoga = createYoga({ schema });
-const server = new  createServer(yoga);
-server.listen(4000, () => {
+const yoga = createYoga({ schema, graphiql: {
+    // Use WebSockets in GraphiQL
+    subscriptionsProtocol: 'WS'
+  } });
+const httpServer = new  createServer(yoga);
+
+
+// Create WebSocket server instance from our Node server
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: yoga.graphqlEndpoint
+  });
+
+// Integrate Yoga's Envelop instance and NodeJS server with graphql-ws
+useServer(
+    {
+      execute: (args) => args.rootValue.execute(args),
+      subscribe: (args) => args.rootValue.subscribe(args),
+      onSubscribe: async (ctx, msg) => {
+        const { schema, execute, subscribe, contextFactory, parse, validate } = yoga.getEnveloped({
+          ...ctx,
+          req: ctx.extra.request,
+          socket: ctx.extra.socket,
+          params: msg.payload
+        })
+   
+        const args = {
+          schema,
+          operationName: msg.payload.operationName,
+          document: parse(msg.payload.query),
+          variableValues: msg.payload.variables,
+          contextValue: await contextFactory(),
+          rootValue: {
+            execute,
+            subscribe
+          }
+        }
+   
+        const errors = validate(args.schema, args.document)
+        if (errors.length) return errors
+        return args
+      }
+    },
+    wsServer
+  )
+
+
+httpServer.listen(4000, () => {
      logger.info('Server is running on http://localhost:4000/graphql')
 });
