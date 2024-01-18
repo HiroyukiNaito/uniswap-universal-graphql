d5c53dcfce14901b7e6673e3c78b1d9e48a09c19
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-01-18 23:37:11 +0900

    Added index

--- model/txnPools.js
+++ model/txnPools.js
@@ -1,25 +1,25 @@
 const expireTime = parseInt(process.env.EXPIRE_TIME ?? 2592000);
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const l1_txnPoolDetails = new Schema({
-        provider: { type: Object},
-        blockNumber:  { type: Number},
-        blockHash: { type: String},
-        hash: { type: String, unique: true, required: true},
-        type:{ type: Number},
-        to:  { type: String},
-        from:  { type: String},
-        nonce: { type: Number},
-        gasLimit: { type: String},
-        gasPrice: { type: String},
-        maxPriorityFeePerGas: { type: String},
-        maxFeePerGas: { type: String},
-        data: { type: String},
-        value: { type: String},
-        chainId: { type: String},
-        signature: { type: Object},
-        accessList: { type: Array},
-        decodedData: { type: Object},
-        createdAt:  { type: Date, index: true, expires: expireTime}
+  provider: { type: Object },
+  blockNumber: { type: Number },
+  blockHash: { type: String },
+  hash: { type: String, unique: true, required: true },
+  type: { type: Number },
+  to: { type: String },
+  from: { type: String },
+  nonce: { type: Number },
+  gasLimit: { type: String },
+  gasPrice: { type: String },
+  maxPriorityFeePerGas: { type: String },
+  maxFeePerGas: { type: String },
+  data: { type: String },
+  value: { type: String },
+  chainId: { type: String },
+  signature: { type: Object },
+  accessList: { type: Array },
+  decodedData: { type: Object },
+  createdAt: { type: Date, index: true, expires: expireTime },
 });
-module.exports = mongoose.model('l1_txnPools', l1_txnPoolDetails);
+module.exports = mongoose.model("l1_txnPools", l1_txnPoolDetails);
